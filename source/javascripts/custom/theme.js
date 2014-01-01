3499cb2659141cedd075b4356b61de7528ce28fe
Bao Pham <gbaopham@gmail.com>
2014-01-01 09:09:08 -0800

    Have a limit for the Vim current line value

--- source/javascripts/custom/theme.js
+++ source/javascripts/custom/theme.js
@@ -3,9 +3,10 @@ function vimScrollUpdate() {
   var scrollAmount = $(window).scrollTop(),
       documentHeight = $(document).height(),
       scrollPercent = (scrollAmount / documentHeight) * 100;
+      currentLine = scrollAmount > 100000000 ? "&infin;" : scrollAmount.toFixed(0);
 
   $('.vim-line-percent').text(scrollPercent.toFixed(0) + '%'); 
-  $('.vim-line-current .vim-line-row').text((scrollAmount).toFixed(0));
+  $('.vim-line-current .vim-line-row').html(currentLine);
 }
 $(document).ready(function() {
   vimScrollUpdate();
