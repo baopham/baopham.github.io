ae7a60645345062a15109c3e7fd7ca3422801ea1
Bao Pham <gbaopham@gmail.com>
2014-01-03 00:47:51 -0800

    Correct scroll percent calculation

--- source/javascripts/custom/theme.js
+++ source/javascripts/custom/theme.js
@@ -2,11 +2,12 @@ function vimScrollUpdate() {
   // grab the scroll amount and the window height
   var scrollAmount = $(window).scrollTop(),
       documentHeight = $(document).height(),
-      scrollPercent = (scrollAmount / documentHeight) * 100;
-      currentLine = scrollAmount > 100000000 ? "&infin;" : scrollAmount.toFixed(0);
+      winHeight = $(window).height();
+      scrollPercent = (scrollAmount / (documentHeight - winHeight)) * 100;
+      scrollAmount = scrollAmount > 100000000 ? "&infin;" : scrollAmount.toFixed(0);
 
   $('.vim-line-percent').text(scrollPercent.toFixed(0) + '%'); 
-  $('.vim-line-current .vim-line-row').html(currentLine);
+  $('.vim-line-current .vim-line-row').html(scrollAmount);
 }
 $(document).ready(function() {
   vimScrollUpdate();
